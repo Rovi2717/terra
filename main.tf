9179a77799fec9f3e499afa6c032c9e3a3f358a2
Rovi2717 <137065428+Rovi2717@users.noreply.github.com>
2023-10-21 17:51:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
 }
   provider "azurerm" {
   features {}
+subscription_id = "2146ae1f-7d1d-4dbf-828b-54f9ca42f169"
+  client_id       = "8701fd9c-8452-4fd9-af14-2705712a5440"
+  client_secret   = "jtd8Q~5GEfo_4Xn5kXFRK6m-fjdSdL-~ABQJEcZz"
+  tenant_id       = "f542f903-8530-4254-aa59-34aa2dcb3bc3"
   
 }
 
